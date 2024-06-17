1cac5af6517bd7d65d8426af48cf6373655663b6
Robin <robin.tordly@nav.no>
2024-06-17 15:47:56 +0200

    nå kan vi oppdatere task, med status og melding

--- apps/utsjekk/main/utsjekk/routing/TaskRoute.kt
+++ apps/utsjekk/main/utsjekk/routing/TaskRoute.kt
@@ -1,13 +1,18 @@
 package utsjekk.routing
 
+import io.ktor.http.*
 import io.ktor.server.application.*
 import io.ktor.server.auth.*
 import io.ktor.server.auth.jwt.*
+import io.ktor.server.request.*
 import io.ktor.server.response.*
 import io.ktor.server.routing.*
+import libs.postgres.concurrency.transaction
 import libs.task.Status
+import libs.task.TaskDao
 import libs.task.Tasks
 import java.time.LocalDateTime
+import java.util.*
 
 private fun ApplicationCall.navident(): String? {
     return principal<JWTPrincipal>()
@@ -28,5 +33,21 @@ fun Route.tasks() {
 
             call.respond(tasks)
         }
+
+        patch("/{id}") {
+            val id = call.parameters["id"]?.let(UUID::fromString)
+                ?: return@patch call.respond(HttpStatusCode.BadRequest, "mangler påkrevd path parameter 'id'")
+
+            transaction { TaskDao.select(id = id) }.singleOrNull()
+                ?: return@patch call.respond(HttpStatusCode.NotFound, "Fant ikke task med id $id")
+
+            val payload = call.receive<TaskDtoPatch>()
+            Tasks.update(id, payload.status, payload.message)
+        }
     }
 }
+
+data class TaskDtoPatch(
+    val status: Status,
+    val message: String,
+)
