6600f3bcbefc306e26f0ee9fad982623df0c8e6d
Robin <robin.tordly@nav.no>
2024-10-07 11:53:39 +0200

    vi trenger ikke oppdatere og logge leader election hele tiden. Kun ved bytte og maks en gang per min

--- apps/utsjekk/test/fakes/LeaderElectorFake.kt
+++ apps/utsjekk/test/fakes/LeaderElectorFake.kt
@@ -1,13 +1,20 @@
 package fakes
 
+import com.fasterxml.jackson.databind.DeserializationFeature
+import com.fasterxml.jackson.databind.SerializationFeature
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule
+import io.ktor.serialization.jackson.*
 import io.ktor.server.application.*
 import io.ktor.server.engine.*
 import io.ktor.server.netty.*
+import io.ktor.server.plugins.contentnegotiation.*
 import io.ktor.server.response.*
 import io.ktor.server.routing.*
 import port
+import utsjekk.LeaderElectionResponse
 import java.net.InetAddress
 import java.net.URI
+import java.time.LocalDateTime
 
 class LeaderElectorFake : AutoCloseable {
     private val elector = embeddedServer(Netty, port = 4040, module = Application::elector).apply { start() }
@@ -16,10 +23,21 @@ class LeaderElectorFake : AutoCloseable {
 }
 
 private fun Application.elector() {
+    install(ContentNegotiation) {
+        jackson {
+            registerModule(JavaTimeModule())
+            disable(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES)
+            disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS)
+        }
+    }
     routing {
         get {
-            val hostname = InetAddress.getLocalHost().hostName
-            call.respondText(hostname)
+            call.respond(
+                LeaderElectionResponse(
+                    name = InetAddress.getLocalHost().hostName,
+                    last_update = LocalDateTime.now()
+                )
+            )
         }
     }
 }
