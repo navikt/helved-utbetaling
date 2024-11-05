38e8cb63035c6571c6685a1ee5eab8007552cd82
Stephen Ramthun <stephenramthun@gmail.com>
2024-11-05 12:54:40 +0100

    Bruk transaction for å få tak i koblingen til databasen
    Co-authored-by: Martine Enger <marenger@student.matnat.uio.no>
    Co-authored-by: Anders Skifte <anders.skifte@nav.no>

--- apps/utsjekk/test/utsjekk/task/strategies/SjekkStatusStrategyTest.kt
+++ apps/utsjekk/test/utsjekk/task/strategies/SjekkStatusStrategyTest.kt
@@ -15,11 +15,12 @@ import org.junit.jupiter.api.AfterEach
 import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Assertions.assertNull
 import org.junit.jupiter.api.Test
+import utsjekk.iverksetting.IverksettingDao
 import utsjekk.iverksetting.resultat.IverksettingResultater
 import utsjekk.task.Status
-import java.util.*
+import java.time.LocalDateTime
+import java.util.UUID
 
-//@org.junit.jupiter.api.Disabled
 class SjekkStatusStrategyTest {
 
     private val createdTaskIds = mutableListOf<UUID>()
@@ -39,202 +40,208 @@ class SjekkStatusStrategyTest {
 
     @Test
     fun `setter task til COMPLETE når status er KVITTERT_OK`() = runTest(TestRuntime.context) {
-        withContext(TestRuntime.context) {
-            val iverksetting = TestData.domain.iverksetting()
-            val oppdragId = TestData.dto.oppdragId(iverksetting)
-            val oppdragStatus = TestData.dto.oppdragStatus(OppdragStatus.KVITTERT_OK)
-            IverksettingResultater.opprett(iverksetting, null)
-            TestRuntime.oppdrag.statusRespondWith(oppdragId, oppdragStatus)
-            val taskId = Tasks.create(libs.task.Kind.SjekkStatus, oppdragId, null, objectMapper::writeValueAsString)
-            createdTaskIds.add(taskId)
-
-            val task = awaitDatabase {
-                TaskDao.select {
-                    it.id = taskId
-                    it.attempt = 1
-                }.firstOrNull()
-            }
-
-            TestRuntime.oppdrag.awaitStatus(oppdragId)
-
-            assertEquals("", task?.message)
-            assertEquals(1, task?.attempt)
-            assertEquals(Status.COMPLETE.name, task?.status?.name)
-            val resultat = IverksettingResultater.hent(iverksetting)
-            assertEquals(OppdragStatus.KVITTERT_OK, resultat.oppdragResultat?.oppdragStatus)
+        val iverksetting = TestData.domain.iverksetting()
+        val oppdragId = TestData.dto.oppdragId(iverksetting)
+        val oppdragStatus = TestData.dto.oppdragStatus(OppdragStatus.KVITTERT_OK)
+        IverksettingResultater.opprett(iverksetting, null)
+        transaction {
+            IverksettingDao(iverksetting, LocalDateTime.now()).insert()
         }
+        TestRuntime.oppdrag.statusRespondWith(oppdragId, oppdragStatus)
+        val taskId = Tasks.create(libs.task.Kind.SjekkStatus, oppdragId, null, objectMapper::writeValueAsString)
+        createdTaskIds.add(taskId)
+
+        val task = awaitDatabase {
+            TaskDao.select {
+                it.id = taskId
+                it.attempt = 1
+            }.firstOrNull()
+        }
+
+        TestRuntime.oppdrag.awaitStatus(oppdragId)
+
+        assertEquals("", task?.message)
+        assertEquals(1, task?.attempt)
+        assertEquals(Status.COMPLETE.name, task?.status?.name)
+        val resultat = IverksettingResultater.hent(iverksetting)
+        assertEquals(OppdragStatus.KVITTERT_OK, resultat.oppdragResultat?.oppdragStatus)
     }
 
     @Test
-    fun `setter task til MANUAL når status er KVITTERT_MED_MANGLER`() =
-        runTest {
-            withContext(TestRuntime.context) {
-                val iverksetting = TestData.domain.iverksetting()
-                val oppdragId = TestData.dto.oppdragId(iverksetting)
-                val oppdragStatus = TestData.dto.oppdragStatus(OppdragStatus.KVITTERT_MED_MANGLER, "mangelvare")
-                IverksettingResultater.opprett(iverksetting, null)
-                TestRuntime.oppdrag.statusRespondWith(oppdragId, oppdragStatus)
-                val taskId = Tasks.create(libs.task.Kind.SjekkStatus, oppdragId, null, objectMapper::writeValueAsString)
-                createdTaskIds.add(taskId)
+    fun `setter task til MANUAL når status er KVITTERT_MED_MANGLER`() = runTest(TestRuntime.context) {
+        val iverksetting = TestData.domain.iverksetting()
+        val oppdragId = TestData.dto.oppdragId(iverksetting)
+        val oppdragStatus = TestData.dto.oppdragStatus(OppdragStatus.KVITTERT_MED_MANGLER, "mangelvare")
+        IverksettingResultater.opprett(iverksetting, null)
+        transaction {
+            IverksettingDao(iverksetting, LocalDateTime.now()).insert()
+        }
+        TestRuntime.oppdrag.statusRespondWith(oppdragId, oppdragStatus)
+        val taskId = Tasks.create(libs.task.Kind.SjekkStatus, oppdragId, null, objectMapper::writeValueAsString)
+        createdTaskIds.add(taskId)
 
-                val task = awaitDatabase {
-                    TaskDao.select {
-                        it.id = taskId
-                        it.attempt = 1
-                    }.firstOrNull()
-                }
-
-                TestRuntime.oppdrag.awaitStatus(oppdragId)
-
-                assertEquals("mangelvare", task?.message)
-                assertEquals(1, task?.attempt)
-                assertEquals(Status.MANUAL.name, task?.status?.name)
-                val resultat = IverksettingResultater.hent(iverksetting)
-                assertEquals(OppdragStatus.KVITTERT_MED_MANGLER, resultat.oppdragResultat?.oppdragStatus)
-            }
+        val task = awaitDatabase {
+            TaskDao.select {
+                it.id = taskId
+                it.attempt = 1
+            }.firstOrNull()
         }
 
-    @Test
-    fun `setter task til MANUAL når status er KVITTERT_TEKNISK_FEIL`() = runTest {
-        withContext(TestRuntime.context) {
-            val iverksetting = TestData.domain.iverksetting()
-            val oppdragId = TestData.dto.oppdragId(iverksetting)
-            val oppdragStatus = TestData.dto.oppdragStatus(OppdragStatus.KVITTERT_TEKNISK_FEIL, "teknisk")
-            IverksettingResultater.opprett(iverksetting, null)
-            TestRuntime.oppdrag.statusRespondWith(oppdragId, oppdragStatus)
+        TestRuntime.oppdrag.awaitStatus(oppdragId)
 
-            val taskId = Tasks.create(libs.task.Kind.SjekkStatus, oppdragId, null, objectMapper::writeValueAsString)
-            createdTaskIds.add(taskId)
-
-            val task = awaitDatabase {
-                TaskDao.select {
-                    it.id = taskId
-                    it.attempt = 1
-                }.firstOrNull()
-            }
-
-            TestRuntime.oppdrag.awaitStatus(oppdragId)
-
-            assertEquals("teknisk", task?.message)
-            assertEquals(1, task?.attempt)
-            assertEquals(Status.MANUAL.name, task?.status?.name)
-            val resultat = IverksettingResultater.hent(iverksetting)
-            assertEquals(OppdragStatus.KVITTERT_TEKNISK_FEIL, resultat.oppdragResultat?.oppdragStatus)
-        }
+        assertEquals("mangelvare", task?.message)
+        assertEquals(1, task?.attempt)
+        assertEquals(Status.MANUAL.name, task?.status?.name)
+        val resultat = IverksettingResultater.hent(iverksetting)
+        assertEquals(OppdragStatus.KVITTERT_MED_MANGLER, resultat.oppdragResultat?.oppdragStatus)
     }
 
     @Test
-    fun `setter task til MANUAL når status er KVITTERT_FUNKSJONELL_FEIL`() = runTest {
-        withContext(TestRuntime.context) {
-            val iverksetting = TestData.domain.iverksetting()
-            val oppdragId = TestData.dto.oppdragId(iverksetting)
-            val oppdragStatus = TestData.dto.oppdragStatus(OppdragStatus.KVITTERT_FUNKSJONELL_FEIL, "funkis")
-            IverksettingResultater.opprett(iverksetting, null)
-            TestRuntime.oppdrag.statusRespondWith(oppdragId, oppdragStatus)
-            val taskId = Tasks.create(libs.task.Kind.SjekkStatus, oppdragId, null, objectMapper::writeValueAsString)
-            createdTaskIds.add(taskId)
-
-            val task = awaitDatabase {
-                TaskDao.select {
-                    it.id = taskId
-                    it.attempt = 1
-                }.firstOrNull()
-            }
-
-            TestRuntime.oppdrag.awaitStatus(oppdragId)
-
-            assertEquals("funkis", task?.message)
-            assertEquals(1, task?.attempt)
-            assertEquals(Status.MANUAL.name, task?.status?.name)
-            val resultat = IverksettingResultater.hent(iverksetting)
-            assertEquals(OppdragStatus.KVITTERT_FUNKSJONELL_FEIL, resultat.oppdragResultat?.oppdragStatus)
+    fun `setter task til MANUAL når status er KVITTERT_TEKNISK_FEIL`() = runTest(TestRuntime.context) {
+        val iverksetting = TestData.domain.iverksetting()
+        val oppdragId = TestData.dto.oppdragId(iverksetting)
+        val oppdragStatus = TestData.dto.oppdragStatus(OppdragStatus.KVITTERT_TEKNISK_FEIL, "teknisk")
+        IverksettingResultater.opprett(iverksetting, null)
+        transaction {
+            IverksettingDao(iverksetting, LocalDateTime.now()).insert()
         }
+        TestRuntime.oppdrag.statusRespondWith(oppdragId, oppdragStatus)
+
+        val taskId = Tasks.create(libs.task.Kind.SjekkStatus, oppdragId, null, objectMapper::writeValueAsString)
+        createdTaskIds.add(taskId)
+
+        val task = awaitDatabase {
+            TaskDao.select {
+                it.id = taskId
+                it.attempt = 1
+            }.firstOrNull()
+        }
+
+        TestRuntime.oppdrag.awaitStatus(oppdragId)
+
+        assertEquals("teknisk", task?.message)
+        assertEquals(1, task?.attempt)
+        assertEquals(Status.MANUAL.name, task?.status?.name)
+        val resultat = IverksettingResultater.hent(iverksetting)
+        assertEquals(OppdragStatus.KVITTERT_TEKNISK_FEIL, resultat.oppdragResultat?.oppdragStatus)
     }
 
     @Test
-    fun `setter task til MANUAL når status er KVITTERT_UKJENT`() = runTest {
-        withContext(TestRuntime.context) {
-            val iverksetting = TestData.domain.iverksetting()
-            val oppdragId = TestData.dto.oppdragId(iverksetting)
-            val oppdragStatus = TestData.dto.oppdragStatus(OppdragStatus.KVITTERT_UKJENT)
-            IverksettingResultater.opprett(iverksetting, null)
-            TestRuntime.oppdrag.statusRespondWith(oppdragId, oppdragStatus)
-            val taskId = Tasks.create(libs.task.Kind.SjekkStatus, oppdragId, null, objectMapper::writeValueAsString)
-            createdTaskIds.add(taskId)
-
-            val task = awaitDatabase {
-                TaskDao.select {
-                    it.id = taskId
-                    it.attempt = 1
-                }.firstOrNull()
-            }
-
-            TestRuntime.oppdrag.awaitStatus(oppdragId)
-
-            assertEquals("Ukjent kvittering fra OS", task?.message)
-            assertEquals(1, task?.attempt)
-            assertEquals(Status.MANUAL.name, task?.status?.name)
-            val resultat = IverksettingResultater.hent(iverksetting)
-            assertEquals(OppdragStatus.KVITTERT_UKJENT, resultat.oppdragResultat?.oppdragStatus)
+    fun `setter task til MANUAL når status er KVITTERT_FUNKSJONELL_FEIL`() = runTest(TestRuntime.context) {
+        val iverksetting = TestData.domain.iverksetting()
+        val oppdragId = TestData.dto.oppdragId(iverksetting)
+        val oppdragStatus = TestData.dto.oppdragStatus(OppdragStatus.KVITTERT_FUNKSJONELL_FEIL, "funkis")
+        IverksettingResultater.opprett(iverksetting, null)
+        transaction {
+            IverksettingDao(iverksetting, LocalDateTime.now()).insert()
         }
+        TestRuntime.oppdrag.statusRespondWith(oppdragId, oppdragStatus)
+        val taskId = Tasks.create(libs.task.Kind.SjekkStatus, oppdragId, null, objectMapper::writeValueAsString)
+        createdTaskIds.add(taskId)
+
+        val task = awaitDatabase {
+            TaskDao.select {
+                it.id = taskId
+                it.attempt = 1
+            }.firstOrNull()
+        }
+
+        TestRuntime.oppdrag.awaitStatus(oppdragId)
+
+        assertEquals("funkis", task?.message)
+        assertEquals(1, task?.attempt)
+        assertEquals(Status.MANUAL.name, task?.status?.name)
+        val resultat = IverksettingResultater.hent(iverksetting)
+        assertEquals(OppdragStatus.KVITTERT_FUNKSJONELL_FEIL, resultat.oppdragResultat?.oppdragStatus)
     }
 
     @Test
-    fun `oppdaterer antall forsøk når status er LAGT_PÅ_KØ`() = runTest {
-        withContext(TestRuntime.context) {
-            val iverksetting = TestData.domain.iverksetting()
-            val oppdragId = TestData.dto.oppdragId(iverksetting)
-            val oppdragStatus = TestData.dto.oppdragStatus(OppdragStatus.LAGT_PÅ_KØ)
-            IverksettingResultater.opprett(iverksetting, null)
-            TestRuntime.oppdrag.statusRespondWith(oppdragId, oppdragStatus)
-            val taskId = Tasks.create(libs.task.Kind.SjekkStatus, oppdragId, null, objectMapper::writeValueAsString)
-            createdTaskIds.add(taskId)
-
-            val task = awaitDatabase {
-                TaskDao.select {
-                    it.id = taskId
-                    it.attempt = 1
-                }.firstOrNull()
-            }
-
-            TestRuntime.oppdrag.statusRespondWith(oppdragId, oppdragStatus)
-
-            assertEquals(null, task?.message)
-            assertEquals(1, task?.attempt)
-            assertEquals(Status.IN_PROGRESS.name, task?.status?.name)
-            val resultat = IverksettingResultater.hent(iverksetting)
-            assertNull(resultat.oppdragResultat?.oppdragStatus)
+    fun `setter task til MANUAL når status er KVITTERT_UKJENT`() = runTest(TestRuntime.context) {
+        val iverksetting = TestData.domain.iverksetting()
+        val oppdragId = TestData.dto.oppdragId(iverksetting)
+        val oppdragStatus = TestData.dto.oppdragStatus(OppdragStatus.KVITTERT_UKJENT)
+        IverksettingResultater.opprett(iverksetting, null)
+        TestRuntime.oppdrag.statusRespondWith(oppdragId, oppdragStatus)
+        transaction {
+            IverksettingDao(iverksetting, LocalDateTime.now()).insert()
         }
+        val taskId = Tasks.create(libs.task.Kind.SjekkStatus, oppdragId, null, objectMapper::writeValueAsString)
+        createdTaskIds.add(taskId)
+
+        val task = awaitDatabase {
+            TaskDao.select {
+                it.id = taskId
+                it.attempt = 1
+            }.firstOrNull()
+        }
+
+        TestRuntime.oppdrag.awaitStatus(oppdragId)
+
+        assertEquals("Ukjent kvittering fra OS", task?.message)
+        assertEquals(1, task?.attempt)
+        assertEquals(Status.MANUAL.name, task?.status?.name)
+        val resultat = IverksettingResultater.hent(iverksetting)
+        assertEquals(OppdragStatus.KVITTERT_UKJENT, resultat.oppdragResultat?.oppdragStatus)
     }
 
     @Test
-    fun `setter task til FAIL når status er OK_UTEN_UTBETALING`() = runTest {
-        withContext(TestRuntime.context) {
-            val iverksetting = TestData.domain.iverksetting()
-            val oppdragId = TestData.dto.oppdragId(iverksetting)
-            val oppdragStatus = TestData.dto.oppdragStatus(OppdragStatus.OK_UTEN_UTBETALING)
-            IverksettingResultater.opprett(iverksetting, null)
-            TestRuntime.oppdrag.statusRespondWith(oppdragId, oppdragStatus)
-            val taskId = Tasks.create(libs.task.Kind.SjekkStatus, oppdragId, null, objectMapper::writeValueAsString)
-            createdTaskIds.add(taskId)
-
-            val task = awaitDatabase {
-                TaskDao.select {
-                    it.id = taskId
-                    it.attempt = 1
-                }.firstOrNull()
-            }
-
-            TestRuntime.oppdrag.statusRespondWith(oppdragId, oppdragStatus)
-
-            assertEquals(
-                "Status ${OppdragStatus.OK_UTEN_UTBETALING} skal aldri mottas fra utsjekk-oppdrag.",
-                task?.message,
-            )
-            assertEquals(1, task?.attempt)
-            assertEquals(Status.FAIL.name, task?.status?.name)
-            val resultat = IverksettingResultater.hent(iverksetting)
-            assertNull(resultat.oppdragResultat?.oppdragStatus)
+    fun `oppdaterer antall forsøk når status er LAGT_PÅ_KØ`() = runTest(TestRuntime.context) {
+        val iverksetting = TestData.domain.iverksetting()
+        val oppdragId = TestData.dto.oppdragId(iverksetting)
+        val oppdragStatus = TestData.dto.oppdragStatus(OppdragStatus.LAGT_PÅ_KØ)
+        IverksettingResultater.opprett(iverksetting, null)
+        transaction {
+            IverksettingDao(iverksetting, LocalDateTime.now()).insert()
         }
+        TestRuntime.oppdrag.statusRespondWith(oppdragId, oppdragStatus)
+        val taskId = Tasks.create(libs.task.Kind.SjekkStatus, oppdragId, null, objectMapper::writeValueAsString)
+        createdTaskIds.add(taskId)
+
+        val task = awaitDatabase {
+            TaskDao.select {
+                it.id = taskId
+                it.attempt = 1
+            }.firstOrNull()
+        }
+
+        TestRuntime.oppdrag.statusRespondWith(oppdragId, oppdragStatus)
+
+        assertEquals(null, task?.message)
+        assertEquals(1, task?.attempt)
+        assertEquals(Status.IN_PROGRESS.name, task?.status?.name)
+        val resultat = IverksettingResultater.hent(iverksetting)
+        assertNull(resultat.oppdragResultat?.oppdragStatus)
+    }
+
+    @Test
+    fun `setter task til FAIL når status er OK_UTEN_UTBETALING`() = runTest(TestRuntime.context) {
+        val iverksetting = TestData.domain.iverksetting()
+        val oppdragId = TestData.dto.oppdragId(iverksetting)
+        val oppdragStatus = TestData.dto.oppdragStatus(OppdragStatus.OK_UTEN_UTBETALING)
+        IverksettingResultater.opprett(iverksetting, null)
+        transaction {
+            IverksettingDao(iverksetting, LocalDateTime.now()).insert()
+        }
+        TestRuntime.oppdrag.statusRespondWith(oppdragId, oppdragStatus)
+        val taskId = Tasks.create(libs.task.Kind.SjekkStatus, oppdragId, null, objectMapper::writeValueAsString)
+        createdTaskIds.add(taskId)
+
+        val task = awaitDatabase {
+            TaskDao.select {
+                it.id = taskId
+                it.attempt = 1
+            }.firstOrNull()
+        }
+
+        TestRuntime.oppdrag.statusRespondWith(oppdragId, oppdragStatus)
+
+        assertEquals(
+            "Status ${OppdragStatus.OK_UTEN_UTBETALING} skal aldri mottas fra utsjekk-oppdrag.",
+            task?.message,
+        )
+        assertEquals(1, task?.attempt)
+        assertEquals(Status.FAIL.name, task?.status?.name)
+        val resultat = IverksettingResultater.hent(iverksetting)
+        assertNull(resultat.oppdragResultat?.oppdragStatus)
     }
 }
