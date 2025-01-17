1ebb75a995d174e2df183808616ee3b43e8358c6
Robin <robin.tordly@nav.no>
2025-01-17 11:17:12 +0100

    utbetaling.utbetaling_id har ikke unique constraint
    Co-authored-by: stephen ramthun <stephen.ramthun@nav.no>
    Co-authored-by: martine enger <marenger@student.matnat.uio.no>

--- apps/utsjekk/migrations/V23__add_utbetaling_to_period_id_table.sql
+++ apps/utsjekk/migrations/V23__add_utbetaling_to_period_id_table.sql
@@ -1,5 +1,5 @@
 CREATE TABLE utbetaling_to_period_id
 (
-    utbetaling_id UUID PRIMARY KEY REFERENCES utbetaling (utbetaling_id),
+    utbetaling_id UUID PRIMARY KEY, -- REFERENCES utbetaling (utbetaling_id),
     period_id     INT
 )
