40af5f23c779d670630ed2e4e6eba1fe42acdf9d
Stephen Ramthun <stephenramthun@gmail.com>
2025-01-28 14:49:19 +0100

    Wrap post i en route

--- apps/simulering/main/simulering/routing/SimuleringRoutes.kt
+++ apps/simulering/main/simulering/routing/SimuleringRoutes.kt
@@ -20,21 +20,23 @@ fun Routing.simulering(
     service: SimuleringService,
     metrics: MeterRegistry,
 ) {
-    post("/simuler") {
-        runCatching {
-            val dto: UtbetalingsoppdragDto = call.receive()
-            val request = SimulerBeregningRequest.from(dto)
-            service.simuler(request)
-        }.onSuccess { sim ->
-            call.respond(HttpStatusCode.OK, sim)
-        }.onFailure { ex ->
-            when (ex) {
-                is IkkeFunnet -> call.respond(HttpStatusCode.NotFound, ex.message!!)
-                is FinnesFraFør -> call.respond(HttpStatusCode.Conflict, ex.message!!)
-                is RequestErUgyldigException -> call.respond(HttpStatusCode.BadRequest, ex.message!!)
-                is OppdragErStengtException -> call.respond(HttpStatusCode.ServiceUnavailable, ex.message!!)
-                else -> call.respond(HttpStatusCode.InternalServerError, ex.message!!).also {
-                    metrics.counter("soap_error_unknown").increment()
+    route("/simulering") {
+        post {
+            runCatching {
+                val dto: UtbetalingsoppdragDto = call.receive()
+                val request = SimulerBeregningRequest.from(dto)
+                service.simuler(request)
+            }.onSuccess { sim ->
+                call.respond(HttpStatusCode.OK, sim)
+            }.onFailure { ex ->
+                when (ex) {
+                    is IkkeFunnet -> call.respond(HttpStatusCode.NotFound, ex.message!!)
+                    is FinnesFraFør -> call.respond(HttpStatusCode.Conflict, ex.message!!)
+                    is RequestErUgyldigException -> call.respond(HttpStatusCode.BadRequest, ex.message!!)
+                    is OppdragErStengtException -> call.respond(HttpStatusCode.ServiceUnavailable, ex.message!!)
+                    else -> call.respond(HttpStatusCode.InternalServerError, ex.message!!).also {
+                        metrics.counter("soap_error_unknown").increment()
+                    }
                 }
             }
         }
