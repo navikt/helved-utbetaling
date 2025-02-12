d095445a5e817e885d79334ac13045402eadccca
Robin <robin.tordly@nav.no>
2025-02-12 22:02:17 +0100

    create diagram for topology

---
+++ apps/abetal/test/abetal/Diagram.kt
@@ -0,0 +1,17 @@
+package abetal
+
+import org.junit.jupiter.api.Test
+import java.io.File
+
+class Diagram {
+
+    @Test
+    fun `generate diagram`() {
+        val mermaid = TestRuntime.kafka.visulize().mermaid().generateDiagram()
+        File("../../dokumentasjon/topology.mmd").apply { writeText(mermaid) }
+
+        val uml = TestRuntime.kafka.visulize().uml()
+        File("../../dokumentasjon/topology.puml").apply { writeText(uml) }
+    }
+}
+
