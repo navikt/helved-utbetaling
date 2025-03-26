449ab9b7408de482d0e94bc74ad8174be0830198
Robin <robin.tordly@nav.no>
2025-03-26 10:43:12 +0100

    bump application_id for å resette topologien

--- apps/abetal/main/abetal/Config.kt
+++ apps/abetal/main/abetal/Config.kt
@@ -1,10 +1,12 @@
 package abetal
 
 import libs.kafka.*
+import libs.utils.env
 import java.util.Properties
 
 data class Config(
     val kafka: StreamsConfig = StreamsConfig(
+        applicationId = "${env<String>("KAFKA_STREAMS_APPLICATION_ID")}v1",
         additionalProperties = Properties().apply {
             put("default.deserialization.exception.handler", ConsumeNextHandler::class.java.name)
         }
