6aa31b17f1c585d33fdaa1496326037599057643
Robin <robin.tordly@nav.no>
2025-04-01 14:52:11 +0200

    bruk DB_URL på nye databaser

--- apps/peisschtappern/main/peisschtappern/Config.kt
+++ apps/peisschtappern/main/peisschtappern/Config.kt
@@ -4,9 +4,13 @@ import java.io.File
 import libs.auth.AzureConfig
 import libs.kafka.StreamsConfig
 import libs.postgres.JdbcConfig
+import libs.utils.env
 
 data class Config(
-    val jdbc: JdbcConfig = JdbcConfig(migrations = listOf(File("migrations"))),
+    val jdbc: JdbcConfig = JdbcConfig(
+        url = env("DB_URL"), // databaser provisjonert etter juni 2024 må bruke denne
+        migrations = listOf(File("migrations")),
+    ),
     val kafka: StreamsConfig = StreamsConfig(),
     val azure: AzureConfig = AzureConfig(),
 )
