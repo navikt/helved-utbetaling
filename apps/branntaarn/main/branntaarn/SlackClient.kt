d36504824a030d8bcefce3d0ce0de01fe3f9478a
robin <robin.tordly@gmail.com>
2025-10-06 14:28:04 +0200

    skal ikke være hashtag

--- apps/branntaarn/main/branntaarn/SlackClient.kt
+++ apps/branntaarn/main/branntaarn/SlackClient.kt
@@ -24,7 +24,7 @@ class SlackClient(
 }
 
 private fun json(brann: Brann, config: Config): String = """{
-  "channel": "#team-hel-ved-alerts",
+  "channel": "team-hel-ved-alerts",
   "blocks": [
     {
       "type": "header",
