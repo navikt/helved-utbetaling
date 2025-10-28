22ac8d49f08b185fda23b09a25e687c95f253a28
robin <robin.tordly@gmail.com>
2025-10-28 11:37:53 +0100

    partitions=3 threads=3 cores=3 pod=1 standby-replica=0 Vi kan ikke ha standby-replica uten å øke enten antall threads+cores eller antall pods. Alternativ 2: resilience (oppetid) 1 standby-replica 4 pods 2 threads 2 cores per pod
    Alternativ 3: lik som alt1 men totalt sett mer minnebruk
    0 standby-replica
    3 pods
    1 threads
    1 core per pod

--- apps/abetal/main/abetal/Config.kt
+++ apps/abetal/main/abetal/Config.kt
@@ -6,6 +6,14 @@ import java.util.Properties
 data class Config(
     val kafka: StreamsConfig = StreamsConfig(
         additionalProperties = Properties().apply {
+            // Vi har 3 partisjoner, for å ha en standby-replica må vi ha 4 poder.
+            // For å bruke 1 pod, kan vi ikke lenger ha noen standby-replicas
+            this[org.apache.kafka.streams.StreamsConfig.NUM_STANDBY_REPLICAS_CONFIG] = 0
+
+            // Vi har 3 partisjoner, hver trenger en tråd på en egen CPU. 
+            // Derfor trenger i 3000m CPU og -XX:ActiveProcessorCount=3
+            this[org.apache.kafka.streams.StreamsConfig.NUM_STREAM_THREADS_CONFIG] = 3
+
             // this[org.apache.kafka.streams.StreamsConfig.DESERIALIZATION_EXCEPTION_HANDLER_CLASS_CONFIG] = ConsumeNextHandler::class.java
             // this[org.apache.kafka.streams.StreamsConfig.PROCESSING_EXCEPTION_HANDLER_CLASS_CONFIG ] = ProcessNextHandler::class.java
         }
