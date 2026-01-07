4db1d8f8f55f2baaffd11f8fa274ce4a9ed8d69e
JorgenNyb <201098578+JorgenNyb@users.noreply.github.com>
2026-01-07 11:04:44 +0100

    Legg til sakid og fagsystem for resterene tabeller

--- apps/peisschtappern/migrations/V20__add_index_columns.sql
+++ apps/peisschtappern/migrations/V20__add_index_columns.sql
@@ -1,14 +1,19 @@
+CREATE OR REPLACE FUNCTION get_xml_field(xml_data TEXT, xpath_query TEXT, ns_prefixes TEXT[][])
+    RETURNS TEXT AS $$
+BEGIN
+    RETURN (xpath(xpath_query, NULLIF(xml_data, '')::xml, ns_prefixes))[1]::text;
+EXCEPTION WHEN OTHERS THEN
+    RETURN NULL;
+END;
+$$ LANGUAGE plpgsql IMMUTABLE;
+
 ALTER TABLE oppdrag
     ADD COLUMN sak_id TEXT GENERATED ALWAYS AS (
-        (xpath('//ns2:oppdrag/oppdrag-110/fagsystemId/text()',
-               NULLIF(record_value, '')::xml,
-               ARRAY [ARRAY ['ns2', 'http://www.trygdeetaten.no/skjema/oppdrag']]))[1]::text)
-        STORED,
+        get_xml_field(record_value, '//ns2:oppdrag/oppdrag-110/fagsystemId/text()', ARRAY [ARRAY ['ns2', 'http://www.trygdeetaten.no/skjema/oppdrag']])
+        ) STORED,
     ADD COLUMN fagsystem TEXT GENERATED ALWAYS AS (
-        (xpath('//ns2:oppdrag/oppdrag-110/kodeFagomraade/text()',
-               NULLIF(record_value, '')::xml,
-               ARRAY [ARRAY ['ns2', 'http://www.trygdeetaten.no/skjema/oppdrag']]))[1]::text)
-        STORED;
+        get_xml_field(record_value, '//ns2:oppdrag/oppdrag-110/kodeFagomraade/text()',ARRAY [ARRAY ['ns2', 'http://www.trygdeetaten.no/skjema/oppdrag']])
+        ) STORED;
 
 ALTER TABLE utbetalinger
     ADD COLUMN sak_id TEXT GENERATED ALWAYS AS (record_value::jsonb ->> 'sakId') STORED,
@@ -20,28 +25,100 @@ ALTER TABLE pending_utbetalinger
 
 ALTER TABLE simuleringer
     ADD COLUMN sak_id TEXT GENERATED ALWAYS AS (
-        (xpath('/ns3:simulerBeregningRequest/request/oppdrag/fagsystemId/text()',
-               NULLIF(record_value, '')::xml,
-               ARRAY[ARRAY['ns3', 'http://nav.no/system/os/tjenester/simulerFpService/simulerFpServiceGrensesnitt']]))[1]::text)
-        STORED,
+        get_xml_field(record_value, '/ns3:simulerBeregningRequest/request/oppdrag/fagsystemId/text()',ARRAY [ARRAY ['ns3', 'http://nav.no/system/os/tjenester/simulerFpService/simulerFpServiceGrensesnitt']])
+        ) STORED,
     ADD COLUMN fagsystem TEXT GENERATED ALWAYS AS (
-        (xpath('/ns3:simulerBeregningRequest/request/oppdrag/kodeFagomraade/text()',
-               NULLIF(record_value, '')::xml,
-               ARRAY[ARRAY['ns3', 'http://nav.no/system/os/tjenester/simulerFpService/simulerFpServiceGrensesnitt']]))[1]::text)
-        STORED;
+        get_xml_field(record_value, '/ns3:simulerBeregningRequest/request/oppdrag/kodeFagomraade/text()',  ARRAY[ARRAY['ns3', 'http://nav.no/system/os/tjenester/simulerFpService/simulerFpServiceGrensesnitt']])
+      ) STORED;
 
 ALTER TABLE kvittering
     ADD COLUMN sak_id TEXT GENERATED ALWAYS AS (
-        (xpath('//ns2:oppdrag/oppdrag-110/fagsystemId/text()',
-               NULLIF(record_value, '')::xml,
-               ARRAY [ARRAY ['ns2', 'http://www.trygdeetaten.no/skjema/oppdrag']]))[1]::text)
-        STORED,
+        get_xml_field(record_value, '//ns2:oppdrag/oppdrag-110/fagsystemId/text()',
+                      ARRAY [ARRAY ['ns2', 'http://www.trygdeetaten.no/skjema/oppdrag']])
+        ) STORED,
     ADD COLUMN fagsystem TEXT GENERATED ALWAYS AS (
-        (xpath('//ns2:oppdrag/oppdrag-110/kodeFagomraade/text()',
-               NULLIF(record_value, '')::xml,
-               ARRAY [ARRAY ['ns2', 'http://www.trygdeetaten.no/skjema/oppdrag']]))[1]::text)
-        STORED;
+        get_xml_field(record_value, '//ns2:oppdrag/oppdrag-110/kodeFagomraade/text()', ARRAY [ARRAY ['ns2', 'http://www.trygdeetaten.no/skjema/oppdrag']])
+        ) STORED;
 
+ALTER TABLE utbetalinger
+    ADD COLUMN sak_id TEXT GENERATED ALWAYS AS (NULLIF(record_value, '')::jsonb ->> 'sakId') STORED,
+    ADD COLUMN fagsystem TEXT GENERATED ALWAYS AS (NULLIF(record_value, '')::jsonb ->> 'fagsystem') STORED;
+
+ALTER TABLE pending_utbetalinger
+    ADD COLUMN sak_id TEXT GENERATED ALWAYS AS (NULLIF(record_value, '')::jsonb ->> 'sakId') STORED,
+    ADD COLUMN fagsystem TEXT GENERATED ALWAYS AS (NULLIF(record_value, '')::jsonb ->> 'fagsystem') STORED;
+
+ALTER TABLE aap
+    ADD COLUMN sak_id TEXT GENERATED ALWAYS AS (NULLIF(record_value, '')::jsonb ->> 'sakId') STORED,
+    ADD COLUMN fagsystem TEXT GENERATED ALWAYS AS (NULLIF(record_value, '')::jsonb ->> 'fagsystem') STORED;
+
+ALTER TABLE aapIntern
+    ADD COLUMN sak_id TEXT GENERATED ALWAYS AS (NULLIF(record_value, '')::jsonb ->> 'sakId') STORED,
+    ADD COLUMN fagsystem TEXT GENERATED ALWAYS AS (NULLIF(record_value, '')::jsonb ->> 'fagsystem') STORED;
+
+ALTER TABLE dp
+    ADD COLUMN sak_id TEXT GENERATED ALWAYS AS (NULLIF(record_value, '')::jsonb ->> 'sakId') STORED,
+    ADD COLUMN fagsystem TEXT GENERATED ALWAYS AS (NULLIF(record_value, '')::jsonb ->> 'fagsystem') STORED;
+
+ALTER TABLE dpIntern
+    ADD COLUMN sak_id TEXT GENERATED ALWAYS AS (NULLIF(record_value, '')::jsonb ->> 'sakId') STORED,
+    ADD COLUMN fagsystem TEXT GENERATED ALWAYS AS (NULLIF(record_value, '')::jsonb ->> 'fagsystem') STORED;
+
+ALTER TABLE ts
+    ADD COLUMN sak_id TEXT GENERATED ALWAYS AS (NULLIF(record_value, '')::jsonb ->> 'sakId') STORED,
+    ADD COLUMN fagsystem TEXT GENERATED ALWAYS AS (NULLIF(record_value, '')::jsonb ->> 'fagsystem') STORED;
+
+ALTER TABLE tsIntern
+    ADD COLUMN sak_id TEXT GENERATED ALWAYS AS (NULLIF(record_value, '')::jsonb ->> 'sakId') STORED,
+    ADD COLUMN fagsystem TEXT GENERATED ALWAYS AS (NULLIF(record_value, '')::jsonb ->> 'fagsystem') STORED;
+
+ALTER TABLE tp
+    ADD COLUMN sak_id TEXT GENERATED ALWAYS AS (NULLIF(record_value, '')::jsonb ->> 'sakId') STORED,
+    ADD COLUMN fagsystem TEXT GENERATED ALWAYS AS (NULLIF(record_value, '')::jsonb ->> 'fagsystem') STORED;
+
+ALTER TABLE tpIntern
+    ADD COLUMN sak_id TEXT GENERATED ALWAYS AS (NULLIF(record_value, '')::jsonb ->> 'sakId') STORED,
+    ADD COLUMN fagsystem TEXT GENERATED ALWAYS AS (NULLIF(record_value, '')::jsonb ->> 'fagsystem') STORED;
+
+ALTER TABLE historisk
+    ADD COLUMN sak_id TEXT GENERATED ALWAYS AS (NULLIF(record_value, '')::jsonb ->> 'sakId') STORED,
+    ADD COLUMN fagsystem TEXT GENERATED ALWAYS AS (NULLIF(record_value, '')::jsonb ->> 'fagsystem') STORED;
+
+ALTER TABLE historiskIntern
+    ADD COLUMN sak_id TEXT GENERATED ALWAYS AS (NULLIF(record_value, '')::jsonb ->> 'sakId') STORED,
+    ADD COLUMN fagsystem TEXT GENERATED ALWAYS AS (NULLIF(record_value, '')::jsonb ->> 'fagsystem') STORED;
+
+ALTER TABLE fk
+    ADD COLUMN sak_id TEXT GENERATED ALWAYS AS (NULLIF(record_value, '')::jsonb ->> 'sakId') STORED,
+    ADD COLUMN fagsystem TEXT GENERATED ALWAYS AS (NULLIF(record_value, '')::jsonb ->> 'fagsystem') STORED;
+
+ALTER TABLE status
+    ADD COLUMN sak_id TEXT GENERATED ALWAYS AS (NULLIF(record_value, '')::jsonb ->> 'sakId') STORED,
+    ADD COLUMN fagsystem TEXT GENERATED ALWAYS AS (NULLIF(record_value, '')::jsonb ->> 'fagsystem') STORED;
+
+ALTER TABLE avstemming
+    ADD COLUMN sak_id TEXT GENERATED ALWAYS AS (NULLIF(record_value, '')::jsonb ->> 'sakId') STORED,
+    ADD COLUMN fagsystem TEXT GENERATED ALWAYS AS (NULLIF(record_value, '')::jsonb ->> 'fagsystem') STORED;
+
+ALTER TABLE dryrun_aap
+    ADD COLUMN sak_id TEXT GENERATED ALWAYS AS (NULLIF(record_value, '')::jsonb ->> 'sakId') STORED,
+    ADD COLUMN fagsystem TEXT GENERATED ALWAYS AS (NULLIF(record_value, '')::jsonb ->> 'fagsystem') STORED;
+
+ALTER TABLE dryrun_tp
+    ADD COLUMN sak_id TEXT GENERATED ALWAYS AS (NULLIF(record_value, '')::jsonb ->> 'sakId') STORED,
+    ADD COLUMN fagsystem TEXT GENERATED ALWAYS AS (NULLIF(record_value, '')::jsonb ->> 'fagsystem') STORED;
+
+ALTER TABLE dryrun_ts
+    ADD COLUMN sak_id TEXT GENERATED ALWAYS AS (NULLIF(record_value, '')::jsonb ->> 'sakId') STORED,
+    ADD COLUMN fagsystem TEXT GENERATED ALWAYS AS (NULLIF(record_value, '')::jsonb ->> 'fagsystem') STORED;
+
+ALTER TABLE dryrun_dp
+    ADD COLUMN sak_id TEXT GENERATED ALWAYS AS (NULLIF(record_value, '')::jsonb ->> 'sakId') STORED,
+    ADD COLUMN fagsystem TEXT GENERATED ALWAYS AS (NULLIF(record_value, '')::jsonb ->> 'fagsystem') STORED;
+
+ALTER TABLE saker
+    ADD COLUMN sak_id TEXT GENERATED ALWAYS AS (NULLIF(record_key, '')::jsonb ->> 'sakId') STORED,
+    ADD COLUMN fagsystem TEXT GENERATED ALWAYS AS (NULLIF(record_key, '')::jsonb ->> 'fagsystem') STORED;
 
 CREATE INDEX oppdrag_lookup_idx ON oppdrag(sak_id, fagsystem);
 CREATE INDEX kvittering_lookup_idx ON kvittering(sak_id, fagsystem);
