1f80717ce65f80200cfbb10bc72398eb37599c9c
robin <robin.tordly@gmail.com>
2026-03-03 12:40:55 +0100

    ferdigstilt aap tester

--- apps/abetal/test/abetal/consumers/AapTest.kt
+++ apps/abetal/test/abetal/consumers/AapTest.kt
@@ -107,6 +107,30 @@ class AapTest : ConsumerTestBase() {
         val uid2 = aapUId(sid.id, meldeperiode2, StønadTypeAAP.AAP_UNDER_ARBEIDSAVKLARING)
         val uid3 = aapUId(sid.id, meldeperiode3, StønadTypeAAP.AAP_UNDER_ARBEIDSAVKLARING)
 
+        val expectedUtbetaling1 = utbetaling(
+            action = Action.CREATE,
+            uid = uid1,
+            originalKey = transactionId,
+            sakId = sid,
+            behandlingId = bid,
+            fagsystem = Fagsystem.AAP,
+            lastPeriodeId = PeriodeId(), // placeholder
+            stønad = StønadTypeAAP.AAP_UNDER_ARBEIDSAVKLARING,
+            vedtakstidspunkt = LocalDateTime.now(),
+            beslutterId = Navident("kelvin"),
+            saksbehandlerId = Navident("kelvin"),
+            personident = Personident("12345678910")
+        ) {
+            periode(7.jun, 20.jun, 553u, 1077u)
+        }
+        val expectedUtbetaling2 = expectedUtbetaling1.copy(
+            uid = uid2,
+            perioder = periode(8.jul, 19.jul, 779u, 2377u)
+        )
+        val expectedUtbetaling3 = expectedUtbetaling1.copy(
+            uid = uid3,
+            perioder = periode(7.aug, 20.aug, 3000u, 3133u),
+        )
         TestRuntime.topics.aap.produce(transactionId) {
             Aap.utbetaling(sid.id, bid.id) {
                 meldekort(meldeperiode1, 7.jun, 20.jun, 553u, 1077u)
@@ -125,45 +149,33 @@ class AapTest : ConsumerTestBase() {
 
         val oppdrag = TestRuntime.topics.oppdrag.assertThat()
             .has(transactionId)
-            .with(transactionId) {
-                assertEquals("1", it.oppdrag110.kodeAksjon)
-                assertEquals("NY", it.oppdrag110.kodeEndring)
-                assertEquals("AAP", it.oppdrag110.kodeFagomraade)
-                assertEquals(sid.id, it.oppdrag110.fagsystemId)
-                assertEquals("MND", it.oppdrag110.utbetFrekvens)
-                assertEquals("12345678910", it.oppdrag110.oppdragGjelderId)
-                assertEquals("kelvin", it.oppdrag110.saksbehId)
-                assertEquals(3, it.oppdrag110.oppdragsLinje150s.size)
-
-                it.oppdrag110.oppdragsLinje150s[0].let {
-                    assertNull(it.refDelytelseId)
-                    assertEquals("NY", it.kodeEndringLinje)
-                    assertEquals(bid.id, it.henvisning)
-                    assertEquals("AAPOR", it.kodeKlassifik)
-                    assertEquals(553, it.sats.toLong())
-                    assertEquals(1077, it.vedtakssats157.vedtakssats.toLong())
-                    assertEquals(it.datoVedtakFom, it.datoKlassifikFom)
-                }
-
-                it.oppdrag110.oppdragsLinje150s[1].let {
-                    assertNull(it.refDelytelseId)
-                    assertEquals("NY", it.kodeEndringLinje)
-                    assertEquals(bid.id, it.henvisning)
-                    assertEquals("AAPOR", it.kodeKlassifik)
-                    assertEquals(779, it.sats.toLong())
-                    assertEquals(2377, it.vedtakssats157.vedtakssats.toLong())
-                    assertEquals(it.datoVedtakFom, it.datoKlassifikFom)
-                }
-
-                it.oppdrag110.oppdragsLinje150s[2].let {
-                    assertNull(it.refDelytelseId)
-                    assertEquals("NY", it.kodeEndringLinje)
-                    assertEquals(bid.id, it.henvisning)
-                    assertEquals("AAPOR", it.kodeKlassifik)
-                    assertEquals(3000, it.sats.toLong())
-                    assertEquals(3133, it.vedtakssats157.vedtakssats.toLong())
-                    assertEquals(it.datoVedtakFom, it.datoKlassifikFom)
-                }
+            .with(transactionId) { oppdrag ->
+                oppdrag.assertBasics("NY", "AAP", sid.id, expectedLines = 3)
+                assertEquals("kelvin", oppdrag.oppdrag110.saksbehId)
+                oppdrag.oppdrag110.oppdragsLinje150s[0].assertLine(
+                    kodeEndringLinje = "NY",
+                    henvisning = bid.id,
+                    kodeKlassifik = "AAPOR",
+                    sats = 553,
+                    vedtakssats = 1077,
+                    refDelytelseId = null
+                )
+                oppdrag.oppdrag110.oppdragsLinje150s[1].assertLine(
+                    kodeEndringLinje = "NY",
+                    henvisning = bid.id,
+                    kodeKlassifik = "AAPOR",
+                    sats = 779,
+                    vedtakssats = 2377,
+                    refDelytelseId = null
+                )
+                oppdrag.oppdrag110.oppdragsLinje150s[2].assertLine(
+                    kodeEndringLinje = "NY",
+                    henvisning = bid.id,
+                    kodeKlassifik = "AAPOR",
+                    sats = 3000,
+                    vedtakssats = 3133,
+                    refDelytelseId = null
+                )
             }
             .get(transactionId)
 
@@ -172,63 +184,15 @@ class AapTest : ConsumerTestBase() {
         TestRuntime.topics.utbetalinger.assertThat()
             .has(uid1.toString(), size = 1)
             .with(uid1.toString()) {
-                val expected = utbetaling(
-                    action = Action.CREATE,
-                    uid = uid1,
-                    originalKey = transactionId,
-                    sakId = sid,
-                    behandlingId = bid,
-                    fagsystem = Fagsystem.AAP,
-                    lastPeriodeId = it.lastPeriodeId,
-                    stønad = StønadTypeAAP.AAP_UNDER_ARBEIDSAVKLARING,
-                    vedtakstidspunkt = it.vedtakstidspunkt,
-                    beslutterId = Navident("kelvin"),
-                    saksbehandlerId = Navident("kelvin"),
-                    personident = Personident("12345678910")
-                ) {
-                    periode(7.jun, 20.jun, 553u, 1077u)
-                }
-                assertEquals(expected, it)
+                assertUtbetaling(expectedUtbetaling1, it)
             }
             .has(uid2.toString())
             .with(uid2.toString()) {
-                val expected = utbetaling(
-                    action = Action.CREATE,
-                    uid = uid2,
-                    originalKey = transactionId,
-                    sakId = sid,
-                    behandlingId = bid,
-                    fagsystem = Fagsystem.AAP,
-                    lastPeriodeId = it.lastPeriodeId,
-                    stønad = StønadTypeAAP.AAP_UNDER_ARBEIDSAVKLARING,
-                    vedtakstidspunkt = it.vedtakstidspunkt,
-                    beslutterId = Navident("kelvin"),
-                    saksbehandlerId = Navident("kelvin"),
-                    personident = Personident("12345678910")
-                ) {
-                    periode(8.jul, 19.jul, 779u, 2377u)
-                }
-                assertEquals(expected, it)
+                assertUtbetaling(expectedUtbetaling2, it)
             }
             .has(uid3.toString())
             .with(uid3.toString()) {
-                val expected = utbetaling(
-                    action = Action.CREATE,
-                    uid = uid3,
-                    originalKey = transactionId,
-                    sakId = sid,
-                    behandlingId = bid,
-                    fagsystem = Fagsystem.AAP,
-                    lastPeriodeId = it.lastPeriodeId,
-                    stønad = StønadTypeAAP.AAP_UNDER_ARBEIDSAVKLARING,
-                    vedtakstidspunkt = it.vedtakstidspunkt,
-                    beslutterId = Navident("kelvin"),
-                    saksbehandlerId = Navident("kelvin"),
-                    personident = Personident("12345678910")
-                ) {
-                    periode(7.aug, 20.aug, 3000u, 3133u)
-                }
-                assertEquals(expected, it)
+                assertUtbetaling(expectedUtbetaling3, it)
             }
     }
 
@@ -370,22 +334,16 @@ class AapTest : ConsumerTestBase() {
         val oppdrag = TestRuntime.topics.oppdrag.assertThat()
             .has(transactionId2)
             .with(transactionId2) { oppdrag ->
-                assertEquals("1", oppdrag.oppdrag110.kodeAksjon)
-                assertEquals("ENDR", oppdrag.oppdrag110.kodeEndring)
-                assertEquals("AAP", oppdrag.oppdrag110.kodeFagomraade)
-                assertEquals(sid.id, oppdrag.oppdrag110.fagsystemId)
-                assertEquals("12345678910", oppdrag.oppdrag110.oppdragGjelderId)
+                oppdrag.assertBasics("ENDR", "AAP", sid.id, expectedLines = 1)
                 assertEquals("kelvin", oppdrag.oppdrag110.saksbehId)
-                assertEquals(1, oppdrag.oppdrag110.oppdragsLinje150s.size)
-                assertEquals(periodeId.toString(), oppdrag.oppdrag110.oppdragsLinje150s[0].refDelytelseId)
-
-                oppdrag.oppdrag110.oppdragsLinje150s[0].let {
-                    assertEquals("NY", it.kodeEndringLinje)
-                    assertEquals("AAPOR", it.kodeKlassifik)
-                    assertEquals(80, it.sats.toLong())
-                    assertEquals(100, it.vedtakssats157.vedtakssats.toLong())
-                    assertEquals(it.datoVedtakFom, it.datoKlassifikFom)
-                }
+                oppdrag.oppdrag110.oppdragsLinje150s[0].assertLine(
+                    kodeEndringLinje = "NY",
+                    henvisning = bid.id,
+                    kodeKlassifik = "AAPOR",
+                    sats = 80,
+                    vedtakssats = 100,
+                    refDelytelseId = periodeId.toString()
+                )
             }
             .get(transactionId2)
 
