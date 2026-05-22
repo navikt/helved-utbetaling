5a9aeb95c581cd76483eb9bfaad4c77c766155cc
robin <robin.tordly@gmail.com>
2026-05-22 10:45:30 +0200

    repliker abetal (1 pod per partition) #540

--- apps/abetal/main/abetal/Config.kt
+++ apps/abetal/main/abetal/Config.kt
@@ -10,14 +10,6 @@ data class Config(
     val utsjekk: URL = URI(env("UTSJEKK_HOST", "http://utsjekk")).toURL(),
     val kafka: StreamsConfig = StreamsConfig(
         additionalProperties = Properties().apply {
-            // Vi har 3 partisjoner, for å ha en standby-replica må vi ha 4 poder.
-            // For å bruke 1 pod, kan vi ikke lenger ha noen standby-replicas
-            this[org.apache.kafka.streams.StreamsConfig.NUM_STANDBY_REPLICAS_CONFIG] = 0
-
-            // Vi har 3 partisjoner, hver trenger en tråd på en egen CPU. 
-            // Derfor trenger i 3000m CPU og -XX:ActiveProcessorCount=3
-            this[org.apache.kafka.streams.StreamsConfig.NUM_STREAM_THREADS_CONFIG] = 3
-
             // Publiser statusmeldinger når vi ikke klarer å prosessere meldinger
             this[org.apache.kafka.streams.StreamsConfig.PROCESSING_EXCEPTION_HANDLER_CLASS_CONFIG] = StatusOnProcessingErrorHandler::class.java
 
