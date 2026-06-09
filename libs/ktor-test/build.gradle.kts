4a740508f8c76f273afabe24ebd336f8742ad8ad
JorgenNyb <201098578+JorgenNyb@users.noreply.github.com>
2026-06-09 12:39:18 +0200

    #546 libs:ktor-test, jackson → kotlinx.
    Signed-off-by: JorgenNyb <201098578+JorgenNyb@users.noreply.github.com>

--- libs/ktor-test/build.gradle.kts
+++ libs/ktor-test/build.gradle.kts
@@ -1,12 +1,10 @@
 val ktorVersion = "3.4.2"
 
 dependencies {
-    api(project(":libs:jackson"))
     api(project(":libs:utils"))
     api("io.ktor:ktor-client-cio:$ktorVersion")
     api("io.ktor:ktor-client-content-negotiation:$ktorVersion")
+    api("io.ktor:ktor-serialization-kotlinx-json:$ktorVersion")
     api("io.ktor:ktor-server-netty:$ktorVersion")
     api("io.ktor:ktor-server-content-negotiation:$ktorVersion")
-    api("io.ktor:ktor-serialization-jackson:$ktorVersion")
-    api("com.fasterxml.jackson.datatype:jackson-datatype-jsr310:2.21.2")
 }
